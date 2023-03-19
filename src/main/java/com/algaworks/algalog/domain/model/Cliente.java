60e7ded59c32547938c9670ba97276a07d4a132b
Antônia Naelly Freire de Lima <josafadieb@gmail.com>
2023-03-19 18:32:00 -0300

    Aula 1.2: utilizando lombok

---
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -0,0 +1,14 @@
+package com.algaworks.algalog.domain.model;
+
+import lombok.Data;
+
+@Data
+public class Cliente {
+
+	private Long id;
+	private String nome;
+	private String email;
+	private String telefone;
+	
+	
+}
