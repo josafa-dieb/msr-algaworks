a413ee43d6bc65dc90cfdeaecd749808f0719bd5
Naum Josafá Dieb da Silva <josafa.silva@fitbank.com.br>
2023-03-31 16:50:46 -0300

    Aula 2.3: Implementando o CRUD de cliente

--- src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
+++ src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
@@ -1,6 +1,6 @@
 package com.algaworks.algalog.domain.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +10,5 @@ import com.algaworks.algalog.domain.model.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
-	List<Cliente> findByNome(String nome);
-	
+	Optional<Cliente> findById(Long id);
 }
