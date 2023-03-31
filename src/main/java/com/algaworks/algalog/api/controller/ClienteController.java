b196fd8792f4e519f6d3324c7cbe66fe06304c48
Naum Josafá Dieb da Silva <josafa.silva@fitbank.com.br>
2023-03-31 15:47:46 -0300

    Aula 2.2: Usando Spring Data JPA

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -2,25 +2,30 @@ package com.algaworks.algalog.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
+
+import lombok.AllArgsConstructor;
 
 
 @RestController
-
+@AllArgsConstructor
 public class ClienteController {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+	
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{nome}")
+	public List<Cliente> listar(String nome) {
+		return clienteRepository.findByNome(nome);
 	}
 	
 }
