34d1d1f631ec2ff5d0bd9c8fa035b2615c3ecb8c
Josafa Dieb <josafadieb@gmail.com>
2023-04-01 15:32:01 -0300

    Aula 2.4: validando com bean validation

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,10 +22,14 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotBlank
 	private String nome;
 	
+	@NotBlank
+	@Email
 	private String email;
 	
+	@NotBlank
 	private String telefone;
 	
 }
